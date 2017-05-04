5f9fbc17147a2553b3180373cb2b26114d52b3d0
enricobacis <enrico.bacis@gmail.com>
2017-05-04 16:59:29 +0200

    add title and total row

--- src/markets.py
+++ src/markets.py
@@ -1,4 +1,6 @@
 from collections import namedtuple
+from itertools import groupby
+from operator import itemgetter
 from tabulate import tabulate
 from poloniex import Poloniex
 from six import iteritems, print_
@@ -30,9 +32,23 @@ def get_results(polo, market='all'):
         delta = value - invested
         yield Result(market, amount, value, delta)
 
+def base_market(result):
+    return result.market.partition('_')[0]
 
 if __name__ == "__main__":
     polo = Poloniex(apikey, secret)
     results = [result for result in get_results(polo) if result.value > 10e-4]
-    results.sort(key=lambda result: result.delta, reverse=True)
-    print_(tabulate(results, headers=Result._fields))
+
+    for key, group in groupby(sorted(results), key=base_market):
+        group = sorted(group, key=lambda result: result.delta, reverse=True)
+        last = ('', '', sum(x[2] for x in group), sum(x[3] for x in group))
+        rows = tabulate(group + [last], headers=Result._fields).split('\n')
+
+        width = len(rows[0])
+        print_('\n' + (' %s ' % key).center(width, '='))
+
+        last = rows.pop(-1)
+        print_(*rows, sep='\n')
+        print_(rows[1])
+        print_(last)
+        print_('=' * width + '\n')
