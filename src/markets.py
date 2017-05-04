500cbcca774d2c2b702595d3dbac49bee44fc4fe
enricobacis <enrico.bacis@gmail.com>
2017-05-04 15:45:14 +0200

    use namedtuple for Result

--- src/markets.py
+++ src/markets.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from tabulate import tabulate
 from poloniex import Poloniex
 
@@ -6,6 +7,8 @@ try:
 except ImportError:
     print 'error: rename src/config.py.changeme in src/config.py and edit it.'
 
+
+Result = namedtuple('Result', ['market', 'shares', 'value', 'delta'])
 
 def get_results(polo, market='all'):
     history = polo.returnTradeHistory(currencyPair=market, start=0, end=2**32-1)
@@ -24,11 +27,11 @@ def get_results(polo, market='all'):
         current = ticker.get(market, {}).get('last', 0.0)
         value = (amount * current)
         delta = value - invested
-        yield (market, amount, value, delta)
+        yield Result(market, amount, value, delta)
 
 
 if __name__ == "__main__":
     polo = Poloniex(apikey, secret)
-    results = [(m, a, v, d) for (m, a, v, d) in get_results(polo) if v > 10e-4]
-    results.sort(key=lambda (m, a, v, d): d, reverse=True)
-    print tabulate(results, headers=['market', 'shares', 'value', 'delta'])
+    results = [result for result in get_results(polo) if result.value > 10e-4]
+    results.sort(key=lambda result: result.delta, reverse=True)
+    print tabulate(results, headers=Result._fields)
